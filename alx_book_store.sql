c4d711bb1581f05b26318f33f4dbea50cdf50383
Zelalem <zzele57@gmail.com>
2025-02-02 22:06:35 +0300

    first db

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -25,5 +25,5 @@ CREATE TABLE IF NOT EXISTS Order_Details (
     order_id INT,
     book_id INT,
     quantity DOUBLE NOT NULL CHECK (quantity > 0),
-    FOREIGN KEY (book_id) REFERENCES Orders(order_id) ON DELETE CASCADE,
-    FOREIGN KEY (book_id) REFERENCES Books(book_id) ON DELETE CASCADE);
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id) ON DELETE CASCADE,
+    FOREIGN KEY (order_id) REFERENCES Books(book_id) ON DELETE CASCADE);
