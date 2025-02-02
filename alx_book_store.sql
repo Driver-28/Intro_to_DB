a41f3617973c9fb2b68dacc9f55239c839f20b20
Zelalem <zzele57@gmail.com>
2025-02-02 22:05:20 +0300

    first db

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,26 +1,26 @@
-CREATE DATABASE alx_book_store;
+CREATE DATABASE IF NOT EXISTS alx_book_store;
 USE alx_book_store;
-CREATE TABLE Books (
+CREATE TABLE IF NOT EXISTS Books (
     book_id int AUTO_INCREMENT Primary Key,
     title VARCHAR(130) NOT NULL,
     author_id INT,
     price DOUBLE NOT NULL CHECK (price >= 0),
     publication_date DATE,
     FOREIGN KEY (author_id) REFERENCES Authors(author_id) ON DELETE SET NULL);
-CREATE TABLE Authors (
+CREATE TABLE IF NOT EXISTS Authors (
     author_id INT AUTHO_INCREMENT PRIMARY KEY,
     author_name VARCHAR(215) NOT NULL);
-CREATE TABLE Customers (
+CREATE TABLE IF NOT EXISTS Customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR(215) NOT NULL,
     email VARCHAR(215) UNIQUE NOT NULL,
     address TEXT);
-CREATE TABLE Orders(
+CREATE TABLE IF NOT EXISTS Orders(
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT,
     order_date DATE NOT NULL DEFAULT (CURDATE()),
  FOREIGN KEY (customer_id) REFERENCES Customers(customer_id) ON DELETE CASCADE);
-CREATE TABLE Order_Details (
+CREATE TABLE IF NOT EXISTS Order_Details (
     orderdetail_id INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
     book_id INT,
